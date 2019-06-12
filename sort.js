0a21f3375fa566028c79a8a25265a3348cbf62a1
Alexander Bakelev <abakelev@mtt.ru>
2019-06-12 16:54:09 +0300

    add yargs

--- sort.js
+++ sort.js
@@ -1,10 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
+const yargs = require('yargs');
 const dir = process.argv.slice(2);
-const sortDir2 = path.join(__dirname, dir[0]);
-const mainDir = path.join(__dirname, dir[1]);
 
+const argv = yargs
+  .usage('Usage: $0 [options]')
+  .help('help')
+  .alias('help', 'h')
+  .version('0.0.1')
+  .alias('version', 'v')
+  .example('$0 --entry ./filesDir -D', '--> Sorting of files in folders')
+  .option('entry', {
+    alias: 'e',
+    describe: 'The path of the source directory',
+    demandOption: true
+  })
+  .option('output', {
+    alias: 'o',
+    describe: 'The path of the output directory',
+    default: '/output'
+  })
+  .epilog('First homework on the node.js course')
+  .argv;
+console.log(argv);
+
+const sortDir2 = path.join(__dirname, argv.entry);
+const mainDir = path.join(__dirname, argv.output);
 fs.access(mainDir, err => {
   if (err) {
     fs.mkdir(mainDir, err => {
