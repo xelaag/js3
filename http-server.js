7b0625b3fa7bb2fa4d29356de9f6522b016e4a67
Bakelev Alexander <bakelev@gmail.com>
2019-06-18 13:08:30 +0300

    add files

--- http-server.js
+++ http-server.js
@@ -1,41 +1,37 @@
-const http = require("http");
+const http = require('http');
+const dotenv = require('dotenv').;
 const port = 3000;
+dotenv.config();
+const intervalTime = process.env.INTERVAL;
+const sendTime = process.env.SENDTIME;
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  if (request.url == "/time") {
+  if (request.url === '/time') {
     const interval = setInterval(() => {
       run();
-    }, 2000);
+    }, intervalTime);
     setTimeout(() => {
       clearInterval(interval);
-      response.end(new Date().toUTCString());
-    }, 5000);
+      response.end('Current DateTime is: ' + new Date().toUTCString());
+    }, sendTime);
   }
-  //   response.end("Go to /time page");
-
-  //   run();
-
-  //   getDT(4000);
-  // var UTCstring = (new Date()).toUTCString();
-  // console.log('UTCstring', UTCstring);
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, err => {
   if (err) {
-    return console.log("Something bad happend", err);
+    return console.log('Something bad happend', err);
   }
   console.log(`server is listening on ${port}`);
 });
 
-function delay() {
+function getUtc () {
   // return await for better async stack trace support in case of errors.
   return new Promise(resolve => resolve(new Date().toUTCString()));
 }
 
-let run = async () => {
-  const r = await delay();
+async function run () {
+  const r = await getUtc();
   console.log(r);
-};
+}
